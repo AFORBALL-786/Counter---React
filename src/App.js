5f827c6cbd41846547be01a2ddc5f48e58c58440
unknown <aforball76@gmail.com>
2023-10-19 20:58:14 +0545

    first Counter App using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function decrementHandler(){
+    setCount(count-1);
+  }
+
+  function incrementHandler(){
+    setCount(count+1);
+  }
+
+  function resetHandler(){
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='title'>
+        Increment And Decrement
+      </div>
+      <div className='button'>
+        <button  onClick={decrementHandler} className='left-button'>
+          -
+        </button>
+        <div className='data'>
+          {count}
+        </div>
+        <button onClick={incrementHandler} className='right-button'>
+          +
+        </button>
+      </div>
+      <div>
+        <button onClick={resetHandler}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
